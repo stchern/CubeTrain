df128c542e481c6b08fe681177f4ff34e0c1c64e
stchern <nastyachernyshovabbk00@mail.ru>
2022-12-14 16:49:27 +0300

    Add sleepers and rails drawing

--- App/main.cpp
+++ App/main.cpp
@@ -1,7 +1,6 @@
 #include "framework/engine.h"
 #include "framework/utils.h"
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/spline.hpp>
+#include "framework/splineutils.h"
 #include <iostream>
 using namespace std;
 using namespace glm;
@@ -15,7 +14,6 @@ using namespace glm;
 
 void goAlongPath(const float *path, float *train_positions);
 void changeTrainPositions(const glm::vec3& start_position, float distance, vector<Object *>& outTrain);
-glm::vec3 catmull_rom_spline_loop(const std::vector<glm::vec3>& control_points, float t);
 
 int main()
 {
@@ -34,6 +32,7 @@ int main()
     Mesh plane_mesh = createPlane();
     Mesh sphere_mesh = createSphere();
     Mesh cube_mesh = createCube();
+    Mesh sleeper_mesh = createSleeper();
 
 //	// create background objects
     Object *plane = engine->createObject(&plane_mesh);
@@ -65,12 +64,12 @@ int main()
         glm::vec3{  8.0f, -0.375f,  7.0f},
 
     };
+
     vector<Object *> points;
     for (int i = 0; i < control_points.size(); i++)
     {
         Object *sphere = engine->createObject(&sphere_mesh);
         sphere->setColor(1, 0, 0);
-//        sphere->setPosition(path[i*3], path[i*3+1], path[i*3+2]);
         sphere->setPosition(control_points[i]);
         sphere->setScale(0.25f);
         points.push_back(sphere);
@@ -78,16 +77,31 @@ int main()
 //    LineDrawer path_drawer(path, points.size(), true);
 
     std::vector<glm::vec3> spline;
-    const float step = 0.00005f;
+    std::vector<glm::vec3> left_rails;
+    std::vector<glm::vec3> right_rails;
+    const float width_rails = 0.5f;
+    float step = 0.00005f;
     for (float t = 0; t < (float)control_points.size(); t += step) {
-        spline.push_back(catmull_rom_spline_loop(control_points, t));
+        glm::vec3 spline_position = SplineUtils::point_on_loop_spline(control_points, t);
+        glm::vec3 spline_grad = SplineUtils::gradient_on_loop_spline(control_points, t);
+        float r = atan2(spline_grad.z, -spline_grad.x);
+        glm::vec3 right_point{width_rails * sin(r) + spline_position.x, spline_position.y, width_rails * cos(r) + spline_position.z};
+        glm::vec3 left_point{-width_rails * sin(r) + spline_position.x, spline_position.y, -width_rails * cos(r) + spline_position.z};
+
+        spline.push_back(spline_position);
+        left_rails.push_back(left_point);
+        right_rails.push_back(right_point);
     }
 
-    LineDrawer path_drawer(spline, true);
+    LineDrawer left_rails_drawer(left_rails, true);
+    LineDrawer right_rails_drawer(right_rails, true);
+    left_rails_drawer.setColor(0.0f, 0.0f, 0.0f);
+    right_rails_drawer.setColor(0.0f, 0.0f, 0.0f);
 
     const float distance_between_cubes = 0.30f;
     glm::vec3 train_position{0.0f, -0.375f,  7.0f};
     vector<Object *> train;
+    vector<Object *> sleepers;
     for (int i = 0; i < 8; i++)
     {
         Object *cube = engine->createObject(&cube_mesh);
@@ -97,6 +111,27 @@ int main()
         cube->setScale(0.25f);
         train.push_back(cube);
     }
+
+    step = 0.07f;
+    for (float t = 0; t < (float)control_points.size(); t += step) {
+        std::vector<glm::vec3> sleeper_points;
+        glm::vec3 spline_position = SplineUtils::point_on_loop_spline(control_points, t);
+        glm::vec3 spline_grad = SplineUtils::gradient_on_loop_spline(control_points, t);
+        float r = atan2(spline_grad.z, -spline_grad.x);
+        glm::vec3 target_position{-2.0f * sin(r), 0.0f, -2.0f * cos(r)};
+
+        float sign = (glm::dot(glm::normalize(target_position), {0.0, 0.0f, -1.0f})) > 0 ? 1.0f : -1.0f;
+        float cos = glm::dot(glm::normalize(target_position), {1.0, 0.0f, 0.0f});
+        float rotationY =  glm::degrees(acos(cos)) ;
+
+        Object *plane = engine->createObject(&plane_mesh);
+        plane->setColor(1.0f, 0.25f, 0.10f);
+        plane->setPosition(spline_position);
+        plane->setRotation(-90.0f,  sign * rotationY, 0.0f);
+        plane->setScale(1.0f, 0.05f, 1.0f);
+    }
+
+
     size_t pointIdx = 0;
     // main loop
     while (!engine->isDone())
@@ -104,9 +139,10 @@ int main()
         engine->update();
         engine->render();
 
-        path_drawer.draw();
-//        changeTrainPositions(glm::vec3{path[pointIdx * 3], path[pointIdx * 3 + 1], path[pointIdx * 3 + 2]}, distance_between_cubes, train);
-        changeTrainPositions(control_points[pointIdx], distance_between_cubes, train);
+//        path_drawer.draw();
+        left_rails_drawer.draw();
+        right_rails_drawer.draw();
+//        changeTrainPositions(control_points[pointIdx], distance_between_cubes, train);
         ++pointIdx %= control_points.size();
         engine->swap();
     }
@@ -130,16 +166,4 @@ void changeTrainPositions(const glm::vec3& start_position, float distance, vecto
         outTrain[idx]->setPosition(next_position);
         next_position.x += distance;
     }
-}
-
-glm::vec3 catmull_rom_spline_loop(const std::vector<glm::vec3>& control_points, float t)
-{
-    // indices of the relevant control points
-    const size_t size = control_points.size();
-    const int idx1 = (int)t;
-    const int idx2 = (idx1 + 1) % size;
-    const int idx3 = (idx2 + 1) % size;
-    const int idx0 = idx1 >= 1 ? idx1 - 1 : size - 1;
-    const float local_t = t - (int)t;
-    return glm::catmullRom(control_points[idx0], control_points[idx1], control_points[idx2], control_points[idx3], local_t);
 }
